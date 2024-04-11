eb6e5e11da5f49460353527ce202b45d0aab9ee5
Brian Blank <beblank@gmail.com>
2024-04-11 17:54:19 -0500

    Update main.go

--- main.go
+++ main.go
@@ -44,6 +44,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	
 	fmt.Println("Successfully Opened users.json")
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
